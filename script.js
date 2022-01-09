7bf744c39058d7235f2120c4d055df5cc2d8511b
reallyhategithub <castleracer@outlook.com>
2022-01-09 11:53:43 -0600

    completed step 3, onto step 4

--- script.js
+++ script.js
@@ -3,16 +3,24 @@
 
 const theGame = (() => {
 
+    let container = document.getElementById('container');
+
 const Gameboard = {
 
     gBoard: ['o','x','o','x']
 }
 
+const appendToBoard = () => {
+    container.append(Gameboard.gBoard);
+};
+
 const Player = (name,level) => {
     console.log(`player name: ${name}, player level: {level}`)    
 }
+
 return {
     Gameboard,
+    appendToBoard,
     Player,
     };
     
