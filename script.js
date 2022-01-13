564cd582c4b50637d61c984a678e2997d0fe27e0
reallyhategithub <castleracer@outlook.com>
2022-01-13 10:13:32 -0600

    Ready!

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 
-//Here we have a module that holds a Gameboard object, and a Player factory function.
 
 const theGame = (() => {
 
@@ -23,7 +22,6 @@ const theGame = (() => {
         let playerName = document.querySelector('#playerName');
         let playerNameBtn = document.querySelector('#playerNameBtn');
         let playBtn = document.querySelector('#playBtn');
-        // let overlay = document.querySelector('#overlay');
         let overlayPlayerWin = document.getElementById('overlayPlayerWin');
         let overlayPlayerName = document.getElementById('overlayPlayerName');
         let playerWinCount = 0;
@@ -31,9 +29,6 @@ const theGame = (() => {
         let tradeShapesBtn = document.getElementById('tradeShapes');
         let p1Shape = document.querySelector('#playerOneShape');
         let p2Shape = document.querySelector('#playerTwoShape');
-        //Let's figure out how to change the shape every time a move is played.
-        //maybe we can add an eventListener to the container div! Upon clicking inside the container, change the shape!
-        ////
         
         playerNameBtn.addEventListener('click', function(){
             let user = playerName.value; 
@@ -43,11 +38,6 @@ const theGame = (() => {
             if(!nameRight.textContent) nameRight.textContent = 'Player Two';
             document.getElementById('overlay').style.display="none";
         })
-
-        // container.addEventListener('click',function(){
-        //     if(shape == 'x') return shape = 'o';
-        //     if(shape == 'o') return shape = 'x';
-        // })
 
        tradeShapesBtn.addEventListener('click',function(){
            p1Shape.textContent == 'x' ? p1Shape.textContent = 'o' : p1Shape.textContent = 'x';
@@ -70,20 +60,9 @@ const theGame = (() => {
             console.log(myArr);
             playBtn.style.display="none";
             tradeShapesBtn.style.display="block";
-            // changeShape();
         });
-        //we're trying a different route, but we could just get the name like ths:
-        // function getName(){
-        //     let userName = prompt(`Player name: `);
-        //     nameLeft.textContent = userName;
-        // }
-        // getName();
     
         const changeShape = () => shape == 'x' ? shape = 'o' : shape = 'x';
-        // const changeShape = () {
-        //     p1Shape == 'x' ? p1Shape = 'o' : p1Shape = 'x';
-        //     p2Shape == 'o' ? p2Shape = 'x' : p2Shape = 'o';
-        // }
         
         sq1.addEventListener('click',function(){
             if(sq1.textContent) return false;
@@ -140,18 +119,9 @@ const theGame = (() => {
             myArr.push(shape)
         })
 
-        function toggleOverlay(){
-
-        }
 
         function xWinner(){
             myArr.push('xWins');
-                    alert('x wins');
-                    // playerWinCount++;
-                    // nameLeft.textContent = `${nameLeft.textContent} = ${playerWinCount}`;
-                    console.log('this is myArr before clearing it');
-                    console.log(myArr);
-                    console.log('this is myArr after clearing it:');
                     myArr.length = 0;
                     console.log(myArr);
 
@@ -161,7 +131,6 @@ const theGame = (() => {
                     else if(p2Shape.textContent == 'x'){
                         overlayPlayerName.textContent = nameRight.textContent + ' wins!';
                     }
-                    // overlayPlayerName.textContent = playerName.value + ' wins!';
                     overlayPlayerWin.style.display="block";
                     
                     window.addEventListener('keydown', function(event){
@@ -170,24 +139,17 @@ const theGame = (() => {
                             playBtn.click();
                         }
                     })
-                    // changeShape();
         }
 
         function oWinner(){
             myArr.push('oWins');
-            alert('o wins');
-            console.log('this is myArr before clearing it');
-            console.log(myArr);
-            console.log('this is myArr after clearing it');
             myArr.length = 0;
-            console.log(myArr);
             if(p2Shape.textContent == 'o'){
                 overlayPlayerName.textContent = nameRight.textContent + ' wins!';
             }
             else if(p1Shape.textContent == 'o'){
                 overlayPlayerName.textContent = nameLeft.textContent + ' wins!';
             }
-            // overlayPlayerName.textContent = `${nameRight.textContent} wins!`;
             overlayPlayerWin.style.display="block";
 
             window.addEventListener('keydown', function(event){
@@ -196,7 +158,6 @@ const theGame = (() => {
                     playBtn.click();
                 }
             })
-            // changeShape();
         }
 
         document.getElementById('closeVictoryPopup').addEventListener('click',function(){
@@ -207,7 +168,7 @@ const theGame = (() => {
 
         container.addEventListener('click',function(){
 
-            //hide trade shapes button:
+            //hide tradeShapes button:
                 tradeShapesBtn.style.display="none";
 
             //Produce reset button:
@@ -271,49 +232,9 @@ const theGame = (() => {
                 myArr.length = 0;
             }
             })
-
-        return {
-            shape,
-            square,
-            sq1,
-            sq2,
-            sq3,
-            sq4,
-            sq5,
-            sq6,
-            sq7,
-            sq8,
-            sq9,
-            changeShape,
-            myArr,
-            // checkEndGame,
-        }
+        
     })();
 
-
-
-const Gameboard = {
-
-    gBoard: ['o','x','o','x']
-}
-
-const appendToBoard = () => {
-    container.append(Gameboard.gBoard);
-
-    
-};
-
-const Player = (name,level) => {
-    console.log(`player name: ${name}, player level: {level}`)    
-}
-
-
-return {
-    Gameboard,
-    myBoard,
-    appendToBoard,
-    Player,
-    };
     
 })();
 
